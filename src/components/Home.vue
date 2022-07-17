98f17aaf3ae9c8cbd802e52420487f8f94a5937e
taco <wkg343@gmail.com>
2022-07-17 18:21:20 +0800

    feat: install sass & vite.config.js alias setting 配置alias别名'@'

---
+++ src/components/Home.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <p >Click on the Vite and Vue logos to learn more</p>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
