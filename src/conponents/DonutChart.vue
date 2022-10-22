fbef71e7446342c341f86589f0ecb25ac2516746
taco <wkg343@gmail.com>
2022-10-22 15:49:52 +0800

    feat: install d3.js

---
+++ src/conponents/DonutChart.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="arcPie-contaier"></div>
+</template>
+
+<script setup>
+  import { onMounted, ref, watchEffect } from "vue";
+  import * as d3 from "d3";
+
+  const props = defineProps({
+    type: {
+      type: String,
+      require: true,
+      default: "text",
+    },
+    name: {
+      type: String,
+      require: true,
+      default: "text",
+    },
+    title: {
+      type: String,
+      default: null,
+    },
+    placeholder: {
+      type: String,
+      default: "請輸入...",
+    },
+  });
+
+  const count = ref(0);
+</script>
+
+<style scoped></style>
---
+++ src/view/D3.vue
@@ -0,0 +1,14 @@
+<template>
+  <!-- This example requires Tailwind CSS v2.0+ -->
+  <div class="py-12 bg-white"></div>
+</template>
+
+<script setup>
+  import { ref } from "vue";
+
+  defineProps({});
+
+  const count = ref(0);
+</script>
+
+<style scoped></style>
